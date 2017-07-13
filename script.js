018bd404b9e0e97ffe577a419d497af090adfce4
Denzel Anderson <zelrsn@gmail.com>
2017-07-13 15:40:59 -0400

    Lab 5: Temperature. Script that changes degrees fahrenheit to celcius.

---
+++ script.js
@@ -0,0 +1,10 @@
+var tempFar, tempCel;
+
+tempFar = 76;
+tempCel = (tempFar-32) * (5/9)
+
+console.log("This script takes a temperature in degrees Fahrenheit and changes it to degrees Celcius.");
+console.log("The formula for the conversion is: Celcius = (Fahrenheit - 32) * (5/9).");
+
+console.log( tempFar + " degrees Fahrenheit = " + tempCel + " degrees Celcius.");
+
